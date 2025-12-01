4bc7927f48d15bce6238f79d714b846b70743bf8
Arvin Buid <arvinbuid01@gmail.com>
2025-12-01 15:57:25 +0800

    feat: create event migration, controller, factory, form request and seed 10 datas

---
+++ app/Models/Event.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /** @use HasFactory<\Database\Factories\EventFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date_time',
+        'location'
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +18,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::firstOrCreate(
-            ['email' => 'test@example.com'],
+            ['email' => 'johndoe@example.com'],
             [
-                'name' => 'Test User',
-                'password' => 'password',
+                'name' => 'John Doe',
+                'password' => Hash::make('johndoe123'),
                 'email_verified_at' => now(),
             ]
         );
+
+        Event::factory()->count(10)->create();
     }
 }
