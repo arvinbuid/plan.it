7a04760d81de0bf81abcc4c3c4117049001d7a8e
Arvin Buid <arvinbuid01@gmail.com>
2025-12-01 20:48:33 +0800

    feat: build events index page

--- resources/js/pages/events/index.tsx
+++ resources/js/pages/events/index.tsx
@@ -1,12 +1,44 @@
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
-export default function Index() {
+export default function Index({ events }) {
     return (
         <AppLayout>
-            <Head title="Dashboard" />
+            <Head title="Events" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                Events will be put here...
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Title</TableHead>
+                            <TableHead>Description</TableHead>
+                            <TableHead>Location</TableHead>
+                            <TableHead>Start Date</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {events.map((event: any) => {
+                            const length = event.description.length;
+                            const description = length > 50 ? event.description.substring(0, 50) + '...' : event.description;
+
+                            return (
+                                <TableRow key={event.id}>
+                                    <TableCell className='font-medium'>{event.title}</TableCell>
+                                    <TableCell>{description}</TableCell>
+                                    <TableCell>{event.location}</TableCell>
+                                    <TableCell>{event.start_date_time}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
             </div>
         </AppLayout>
     );
