056fdf4f73e1fee32fae01daa905bbeb5c538948
Arvin Buid <arvinbuid01@gmail.com>
2025-12-22 22:28:38 +0800

    feat: add upcoming events card section

--- resources/js/pages/Dashboard/DashboardGrid.tsx
+++ resources/js/pages/Dashboard/DashboardGrid.tsx
@@ -16,7 +16,7 @@ interface DashboardGridProps {
 const DashboardGrid = ({ totalEvents, upcomingEvents, ongoingEvents, pastEvents, chartData }: DashboardGridProps) => {
     return (
         <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-            <div className="grid auto-rows-min md:grid-cols-2 xl:grid-cols-4 gap-4">
+            <div className="grid auto-rows-min md:grid-cols-2 xl:grid-cols-5 gap-4">
                 {/* Total Events */}
                 <div className="relative overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <div className="p-6 space-y-2">
@@ -61,11 +61,18 @@ const DashboardGrid = ({ totalEvents, upcomingEvents, ongoingEvents, pastEvents,
                         <p className="font-bold text-4xl text-blue-900 dark:text-gray-200">{pastEvents}</p>
                     </div>
                 </div>
+                <div className="relative overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <div className="p-6">
+                        <h1 className="text-xl font-bold">Upcoming Events</h1>
+                    </div>
+                </div>
             </div>
-            <div className="space-y-2">
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border p-6">
-                    <h1 className="text-2xl font-bold">Events Chart</h1>
-                    <EventsChart data={chartData} />
+            <div className="grid grid-cols-5">
+                <div className="col-span-5 md:col-span-4 space-y-2">
+                    <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border p-6">
+                        <h1 className="text-2xl font-bold">Events Chart</h1>
+                        <EventsChart data={chartData} />
+                    </div>
                 </div>
             </div>
         </div>
