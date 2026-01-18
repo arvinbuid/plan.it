6c5b7a309e38aceff971f1f4acad57591c3153d9
Arvin Buid <arvinbuid01@gmail.com>
2026-01-18 23:09:40 +0800

    feat: build right side of hero section

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -56,17 +56,20 @@ export default function Welcome({
                         )}
                     </nav>
                 </header>
-                {/* Left Side */}
                 <div className='grid grid-cols-12 h-[calc(100vh-150px)]'>
-                    <div className='col-span-8'>
+                    {/* Left Side */}
+                    <div className='col-span-5'>
                         <div className='mt-20 pl-28 space-y-6 max-w-2xl'>
                             <h2 className='font-playfair text-7xl leading-18 tracking-tighter font-medium'>Make every school <br /><span>events</span> <br /><span className='font-light underline decoration-[1.5px] underline-offset-8 text-[#DFBF4A] italic'>nice.</span></h2>
                             <p className='text-lg text-[#626162]'>From the bottom to the summit. Effortlessly manage school events, meetings with a platform design for grandeur.</p>
                             <Link>
                                 <button className='flex items-center gap-3 px-8 py-4 bg-[#531527] text-[#DAC0C8] uppercase text-sm tracking-wider font-semibold'>Get Started <span className='w-2 h-2 rounded-full bg-[#E4C967]'></span></button>
-
                             </Link>
                         </div>
+                    </div>
+                    {/* Right Side */}
+                    <div className='col-span-7 flex items-center'>
+                        <img src='/images/project-showcase.png' alt='Hero Image' className='border-6 border-[#531527] rounded-xl translate-x-32 h-[88%]' />
                     </div>
                 </div>
             </div>
