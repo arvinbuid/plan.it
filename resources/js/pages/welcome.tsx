b5be585b2f8d3ea15f4c1726a504ff0fe354f50c
Arvin Buid <arvinbuid01@gmail.com>
2026-01-18 17:03:04 +0800

    feat: add two fonts and create navbar

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -18,9 +18,9 @@ export default function Welcome({
                     rel="stylesheet"
                 />
             </Head>
-            <div className="flex min-h-screen flex-col bg-[#FDFDFC] p-6 text-[#1b1b18] lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl mx-auto">
-                    <nav className="flex items-center justify-end gap-4">
+            <div className="flex min-h-screen flex-col bg-[#FFFAF5] text-[#3F101E] font-monserrat">
+                <header className="flex items-center justify-between text-3xl not-has-[nav]:hidden bg-[#FFFAF5] h-[100px] shadow-md/5 px-[120px]">
+                    <nav className='flex justify-between w-full'>
                         {auth.user ? (
                             <Link
                                 href={dashboard()}
@@ -30,20 +30,28 @@ export default function Welcome({
                             </Link>
                         ) : (
                             <>
-                                <Link
-                                    href={login()}
-                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                                >
-                                    Log in
-                                </Link>
-                                {canRegister && (
+                                <h1 className='text-3xl font-playfair font-bold text-[#3F101E]'>Laravel + <span className='text-[#746326]'>React</span></h1>
+                                <div className='flex items-center gap-9 font-medium'>
+                                    <Link className='uppercase text-[16px]'><p>Philosophy</p></Link>
+                                    <Link className='uppercase text-[16px]'><p>Features</p></Link>
+                                    <Link className='uppercase text-[16px]'><p>About</p></Link>
+                                </div>
+                                <div className='flex items-center gap-2'>
                                     <Link
-                                        href={register()}
-                                        className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                        href={login()}
+                                        className="inline-block rounded-xs border border-[#746326] text-[#746326] px-6 py-2.5 text-xs leading-normal uppercase font-medium"
                                     >
-                                        Register
+                                        Log in
                                     </Link>
-                                )}
+                                    {canRegister && (
+                                        <Link
+                                            href={register()}
+                                            className="inline-block rounded-xs border border-[#531527] bg-[#531527] text-[#DAC0C8] px-6 py-2.5 text-xs leading-normal uppercase font-medium"
+                                        >
+                                            Register
+                                        </Link>
+                                    )}
+                                </div>
                             </>
                         )}
                     </nav>
