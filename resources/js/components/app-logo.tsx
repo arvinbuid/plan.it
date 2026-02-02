05cbd9ca1f71219542bdd6c504dd264f7b6c749a
Arvin Buid <arvinbuid01@gmail.com>
2026-02-02 16:39:55 +0800

    feat: display correct app logo color depending if dark mode or not

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+
 export default function AppLogo() {
+    const [isDarkMode, setIsDarkMode] = useState(
+        document.documentElement.classList.contains('dark')
+    )
+
+    // Effect to watch changes being made to the DOM tree
+    useEffect(() => {
+        const observer = new MutationObserver(() => {
+            setIsDarkMode(document.documentElement.classList.contains('dark'))
+        })
+
+        observer.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ['class'] // only watch for changes to the 'class' attribute
+        });
+
+        return () => observer.disconnect();
+    }, [])
+
+    const defaultLogo = 'images/logo-default.svg';
+    const darkModeLogo = 'images/logo-dark.svg';
+
     return (
         <>
             <div className="flex aspect-square size-6 items-center justify-center rounded-md text-sidebar-primary-foreground">
-                <img src={'/images/app-logo.svg'} alt='App Logo' />
+                <img src={isDarkMode ? darkModeLogo : defaultLogo} alt='App Logo' />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
